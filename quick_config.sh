6027100d13147a5f538d4ec183abd672fa4af797
Lukas <ldv8434@rit.edu>
2024-03-24 15:45:15 -0400

    add quick_config.sh

---
+++ quick_config.sh
@@ -0,0 +1,7 @@
+git config user.name 'Lukas Vacula'
+git config user.email 'lukas@lvacula.com'
+# To retroactively fix commits, use the following command:
+# git rebase -r --root --exec "git commit --amend --no-edit --reset-author"
+# git push --force
+# **WARNING** This will set *all* commits to the same author.
+
